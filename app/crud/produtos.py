00d86df1700b211ec560c60055c10a663bde18f6
romulogm <romulogm@gmail.com>
2025-06-12 16:56:56 -0300

    Fix

--- app/crud/produtos.py
+++ app/crud/produtos.py
@@ -7,16 +7,21 @@ def get_produtos(db: Session, skip: int = 0, limit: int = 100, feira_id: Optiona
     """
     Busca produtos no banco de dados.
 
-    - Se um `feira_id` é fornecido, retorna apenas os produtos que têm estoque naquela feira.
+    - Se um `feira_id` é fornecido, retorna apenas os produtos que têm estoque
+      naquela feira e cuja quantidade seja maior que zero.
     - Se `feira_id` for None, retorna todos os produtos.
     """
 
     query = db.query(Produto)
 
     if feira_id is not None:
-        query = query.join(Estoque).filter(Estoque.feira_id == feira_id).distinct()
+        query = query.join(Estoque).filter(
+            Estoque.feira_id == feira_id,
+            Estoque.quantidade > 0
+        ).distinct()
 
     return query.offset(skip).limit(limit).all()
+
 
 def get_produto(db: Session, produto_id: int):
     return db.query(Produto).filter(Produto.produto_id == produto_id).first()
